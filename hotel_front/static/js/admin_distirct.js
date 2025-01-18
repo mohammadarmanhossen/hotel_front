0ba40e02eb40343aee15f6fc0c4fff49e1cf2bed
Mohammad Arman <121811060+mohammadarmanhossen@users.noreply.github.com>
2025-01-19 02:04:42 +0600

    Add files via upload

---
+++ hotel_front/static/js/admin_distirct.js
@@ -0,0 +1,30 @@
+const loadAllDistirct= () => {
+    fetch("https://hotel-backend-cmcn.onrender.com/district/")
+      .then((res) => res.json())
+      .then((data) => {
+        const tableBody = document.getElementById("districtTableBody");
+        tableBody.innerHTML = ""; // Clear any existing rows
+        
+        data.forEach((item,index) => {
+          const row = document.createElement("tr");
+          
+          row.innerHTML = `
+            <th scope="row">${index + 1}</th>
+            <td>${item. district_name}</td>
+            <td>${item.slug}</td>
+             <td><button class="btn btn-danger btn-sm">Delete</button></td>
+          `;
+          
+          tableBody.appendChild(row);
+        });
+      })
+      .catch((error) => console.error("Error fetching user data:", error));
+  };
+  
+  
+  // Call the function when the page loads
+loadAllUser();
+loadAllHotel();
+loadAllDistirct();
+loadReview()
+loadDistrictsForForm();
---
+++ hotel_front/static/js/admin_user.js
@@ -0,0 +1,31 @@
+const loadAllUser = () => {
+    fetch("https://hotel-backend-cmcn.onrender.com/client/users/")
+      .then((res) => res.json())
+      .then((data) => {
+        const tableBody = document.getElementById("userTableBody");
+        tableBody.innerHTML = ""; // Clear any existing rows
+        
+        data.forEach((user, index) => {
+          const row = document.createElement("tr");
+          
+          row.innerHTML = `
+            <th scope="row">${index + 1}</th>
+            <td><img src="./Images/man.png" alt="" style="height:50px"></td>
+            <td>${user.username}</td>
+            <td>${user.first_name}</td>
+            <td>${user.last_name}</td>
+            <td>${user.email}</td>
+            <td><button class="btn btn-danger btn-sm" onclick="deleteUser(${user.id})">Delete</button></td>
+          `;
+          tableBody.appendChild(row);
+        });
+      })
+      .catch((error) => console.error("Error fetching user data:", error));
+  };
+
+  // Call the function when the page loads
+loadAllUser();
+loadAllHotel();
+loadAllDistirct();
+loadReview()
+loadDistrictsForForm();
